e2c70eeac7c5aa364ec5451ae8f89aba1650b4ac
U_ssef_09 <youssef.1011011@gmail.com>
2023-09-15 16:40:42 +0200

    initialize frontend with some features

---
+++ frontend/src/component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <div className='containder-fluid' style={{background: "green", padding: "15px 0", color : "#fafafa"}}>
+                <div className='container'>
+                    <h5 style={{color:"#fafafa"}}>TODO LIST </h5>
+                </div>
+            </div>
+        
+        </>
+    )
+}
+
+export default Header
