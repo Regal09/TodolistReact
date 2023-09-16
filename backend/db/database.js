c2733cd90686b7a1fe0e32e3097a4a5fa289cf61
U_ssef_09 <youssef.1011011@gmail.com>
2023-09-16 21:34:18 +0200

    try to connect with render

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,11 +1,16 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: 'mysql-egdadi.alwaysdata.net',
-    user: 'egdadi',
-    password: 'Y_eJ4wnpqdZ;rpc',
-    database: 'egdadi_todolist'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME
 });
+
+//DB_HOST="mysql-egdadi.alwaysdata.net"
+//DB_USERNAME="egdadi"
+//DB_PASSWORD="Y_eJ4wnpqdZ"
+//DB_DBNAME="egdadi_todolist"
 
 connection.connect(error => {
     if(error) throw error;
